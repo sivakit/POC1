ccce474d28d2b029de65b08256eacac59266fddf
root <root@ip-172-31-25-83.us-east-2.compute.internal>
2023-04-07 06:41:55 +0000

    Added install script

---
+++ install.sh
@@ -0,0 +1,18 @@
+#Install VirtualBox:
+sudo apt update -y
+sudo apt-get install docker.io -y
+vi /lib/systemd/system/docker.service
+    ExecStart=/usr/bin/dockerd -H fd:// --containerd=/run/containerd/containerd.sock -H=tcp://0.0.0.0:2375
+systemctl start docker
+sudo usermod -aG docker $(whoami)
+sudo apt install virtualbox -y
+sudo apt-get install conntrack -y
+#Install kubectl:
+sudo apt-get update && sudo apt-get install -y apt-transport-https gnupg2
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubectl
+#Install Minikube:
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
+minikube start --kubernetes-version=v1.21.0 --driver=none
